452aaa9dc22479f4c5530eb6732a8c93ad45f836
Eric Love <elove78@gmail.com>
2015-03-17 12:07:07 -0500

    Complete static pages assignment

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
