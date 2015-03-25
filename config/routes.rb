5dea00047aede3d50bafe1caa375a16d3f59c232
Eric Love <elove78@gmail.com>
2015-03-24 19:02:47 -0500

    Created posts controller and made updates to the index and show html files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :posts
 
-  get 'welcome/about'
-
+  get 'about' => 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
