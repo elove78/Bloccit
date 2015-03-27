5d504eb1ca7361dfa702bfb3c23b613e210d12c6
Eric Love <elove78@gmail.com>
2015-03-27 18:58:24 -0500

    Complete authentication continued assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  
+before_action :flash_attack
+
+  def flash_attack
+    flash[:notice] = "Flash Gordon is Fast"
+  end
+
   protect_from_forgery with: :exception
    before_action :configure_permitted_parameters, if: :devise_controller?
  
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  skip_before_action :flash_attack, only: [:index, :new]
+
   def index
     @posts = Post.all
   end
