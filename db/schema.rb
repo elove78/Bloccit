2e3d190a3904997e2f542d796378fb22d4fe03ce
Eric Love <elove78@gmail.com>
2015-03-27 20:54:05 -0500

    completed associations-checkpoint. Added users_id to posts and reset the database with new associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326134415) do
+ActiveRecord::Schema.define(version: 20150328005656) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20150326134415) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
