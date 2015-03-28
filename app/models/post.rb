f2bc31f6f52d9554ec9ac313eb36b750b1037003
Eric Love <elove78@gmail.com>
2015-03-28 08:59:53 -0500

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   default_scope { order('created_at DESC') }
+
+  scope :ordered_by_title, -> { order("title ASC") }
+
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+
 end
