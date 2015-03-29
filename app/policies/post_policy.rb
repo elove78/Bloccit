554bcc44ddfc894f4886fc869eb9af65fb1d947e
Eric Love <elove78@gmail.com>
2015-03-29 15:38:24 -0500

    updated Gem's, Rails-dom-testing, autoprefixer-rails, bootstrap-sass, jbuilder

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,6 +1,25 @@
 class PostPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
 
-  def index?
-    true
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin? || user.moderator? 
+        scope.all
+      elsif user.owner_of?(post)
+        scope.self
+      else
+        false
+      end
+    end
   end
+
+
+
+
+
 end
