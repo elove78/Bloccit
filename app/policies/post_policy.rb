1c03027f583291c87e321238c34826c54fb5281b
Eric Love <elove78@gmail.com>
2015-03-29 15:58:42 -0500

    corrected and updated post_policy file

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,25 +1,7 @@
 class PostPolicy < ApplicationPolicy
-  class Scope
-    attr_reader :user, :scope
-
-    def initialize(user, scope)
-      @user = user
-      @scope = scope
-    end
-
-    def resolve
-      if user.admin? || user.moderator? 
-        scope.all
-      elsif user.owner_of?(post)
-        scope.self
-      else
-        false
-      end
-    end
+  
+  def index?
+    true
   end
-
-
-
-
 
 end
